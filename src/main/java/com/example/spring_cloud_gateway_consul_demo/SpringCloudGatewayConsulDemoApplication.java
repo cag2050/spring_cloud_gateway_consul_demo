c04961396755bc174269de33b3180558cc23de11
chen_anguo <cag2050@163.com>
2019-09-18 20:06:59 +0800

    project init

---
+++ src/main/java/com/example/spring_cloud_gateway_consul_demo/SpringCloudGatewayConsulDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_cloud_gateway_consul_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayConsulDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayConsulDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_cloud_gateway_consul_demo/SpringCloudGatewayConsulDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.spring_cloud_gateway_consul_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudGatewayConsulDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
